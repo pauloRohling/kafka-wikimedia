2ff508c31f00322d0b2a9dc822391516211424b7
Paulo Rohling <paulohr.freecopias@gmail.com>
2025-07-24 14:09:37 -0300

    fix: adjusting event polling

--- consumer/src/main/java/com/wikimedia/consumer/messaging/WikimediaConsumer.java
+++ consumer/src/main/java/com/wikimedia/consumer/messaging/WikimediaConsumer.java
@@ -4,7 +4,9 @@ import com.wikimedia.consumer.opensearch.Indexes;
 import jakarta.annotation.PreDestroy;
 import java.time.Duration;
 import java.util.List;
+import java.util.Optional;
 import java.util.Properties;
+import java.util.stream.StreamSupport;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
@@ -14,15 +16,12 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.opensearch.action.DocWriteResponse;
 import org.opensearch.action.index.IndexRequest;
 import org.opensearch.action.index.IndexResponse;
-import org.opensearch.action.index.IndexResponse.Builder;
 import org.opensearch.client.RequestOptions;
 import org.opensearch.client.RestHighLevelClient;
 import org.opensearch.common.xcontent.XContentType;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 @Component
 @Slf4j
@@ -33,11 +32,13 @@ public class WikimediaConsumer {
 
     public WikimediaConsumer(RestHighLevelClient openSearchClient) {
         final var properties = new Properties();
-        properties.setProperty(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, "localhost:9094");
+        properties.setProperty(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG,
+            "localhost:29092,localhost:39092,localhost:49092");
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "wikimedia.recentchange.consumer");
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "wikimedia.recentchange.consumergroup");
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
 
         this.kafkaConsumer = new KafkaConsumer<>(properties);
         this.openSearchClient = openSearchClient;
@@ -47,20 +48,21 @@ public class WikimediaConsumer {
     public void listen() {
         this.kafkaConsumer.subscribe(List.of(Topics.RECENT_CHANGE));
 
-        final var records = this.kafkaConsumer.poll(Duration.ofMillis(3000));
+        while (true) {
+            final var records = this.kafkaConsumer.poll(Duration.ofMillis(3000));
 
-        Flux.fromIterable(records)
-            .map(ConsumerRecord::value)
-            .map(this::toIndexRequest)
-            .flatMap(this::send)
-            .map(DocWriteResponse::getId)
-            .doOnNext(id -> log.info("Sent an event to OpenSearch: {}", id))
-            .subscribe();
+            StreamSupport.stream(records.spliterator(), false)
+                .map(ConsumerRecord::value)
+                .map(this::toIndexRequest)
+                .forEach(request -> this.send(request)
+                    .map(DocWriteResponse::getId)
+                    .ifPresent(s -> log.info("Sent an event to OpenSearch: {}", s)));
+        }
     }
 
     @PreDestroy
     public void shutdown() {
-        this.kafkaConsumer.unsubscribe();
+        this.kafkaConsumer.wakeup();
         this.kafkaConsumer.close();
         log.info("Closed consumer of topic '{}'", Topics.RECENT_CHANGE);
     }
@@ -69,15 +71,12 @@ public class WikimediaConsumer {
         return new IndexRequest(Indexes.RECENT_CHANGE).source(value, XContentType.JSON);
     }
 
-    private Mono<IndexResponse> send(IndexRequest request) {
-        return Mono.fromCallable(() -> {
-            try {
-                return this.openSearchClient.index(request, RequestOptions.DEFAULT);
-            } catch (Exception e) {
-                final var builder = new Builder();
-                builder.setId("none");
-                return builder.build();
-            }
-        });
+    private Optional<IndexResponse> send(IndexRequest request) {
+        try {
+            final var response = this.openSearchClient.index(request, RequestOptions.DEFAULT);
+            return Optional.of(response);
+        } catch (Exception e) {
+            return Optional.empty();
+        }
     }
 }
